425de358f1a123f4e5b8d54d70cda2e6d4dda2fa
Phong Nguyen <phongnn@dgroup.co>
2020-08-12 22:09:28 +0700

    moving background

---
+++ final-project/Assets/Script/background_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_move : MonoBehaviour {
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public float moveRange;
+    private Vector3 oldPosition;
+    private GameObject obj;
+    void Start () {
+        obj = gameObject;
+        oldPosition = obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        obj.transform.Translate (new Vector3 (-1 * Time.deltaTime * moveSpeed, 0, 0));
+        if (Vector3.Distance (oldPosition, obj.transform.position) >= moveRange) {
+            obj.transform.position = oldPosition;
+        }
+    }
+}
