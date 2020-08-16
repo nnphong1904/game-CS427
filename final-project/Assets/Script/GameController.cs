55960d6349032271b0ada118d0ab2b5677c088c0
Phong Nguyen <phongnn@dgroup.co>
2020-08-16 21:52:48 +0700

    add start, restart and end game function

---
+++ final-project/Assets/Script/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+    // Start is called before the first frame update
+    bool isEndGame;
+    public GameObject panelEndGame;
+    public Text yourPoint;
+    public Button restartButton;
+    void Start () {
+        Time.timeScale = 0;
+        isEndGame = false;
+        panelEndGame.SetActive (false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown ("Jump") && !isEndGame) {
+            Time.timeScale = 1;
+        }
+    }
+    public void StartGame () {
+        SceneManager.LoadScene (0);
+
+    }
+    public void RestratGame () {
+        StartGame ();
+    }
+    public void EndGame () {
+        Debug.Log ("end game");
+        Time.timeScale = 0;
+        isEndGame = true;
+        panelEndGame.SetActive (true);
+    }
+}
--- final-project/Assets/Script/megaman_jump.cs
+++ final-project/Assets/Script/megaman_jump.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class megaman_jump : MonoBehaviour {
     public float jumpPower;
     GameObject obj;
-    public Collider coll;
+    public GameObject gameController;
     // Start is called before the first frame update
     void Start () {
         obj = gameObject;
-        coll = GetComponent<Collider> ();
-        coll.isTrigger = true;
+        if (gameController == null) {
+            gameController = GameObject.FindGameObjectWithTag ("GameController");
+        }
     }
 
     // Update is called once per frame
     void Update () {
         float currentY = obj.transform.position.y;
-        if (Input.GetButtonDown ("Jump") && currentY < 3) {
+        if (Input.GetButtonDown ("Jump") && currentY < 0) {
             obj.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpPower));
         }
+        if (Input.GetButtonDown ("End") && currentY < 3) {
+            gameController.GetComponent<GameController> ().EndGame ();
+        }
+
     }
-    void OnTriggerEnter (Collider other) {
-        if (other.attachedRigidbody)
-            other.attachedRigidbody.useGravity = false;
+    void EndGame () {
+        gameController.GetComponent<GameController> ().EndGame ();
     }
 }
